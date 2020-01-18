72a4d23feeffa59edde7445698026bcb6b7670ba
AlinaStepanova <stepanovaalina15@gmail.com>
2020-01-18 19:19:16 +0200

    create custom views for squares, draw lines

---
+++ app/src/main/java/com/avs/battleship/views/ComputerSquareView.kt
@@ -0,0 +1,15 @@
+package com.avs.battleship.views
+
+import android.content.Context
+import android.util.AttributeSet
+
+class ComputerSquareView : SquareView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+}
---
+++ app/src/main/java/com/avs/battleship/views/SquareView.kt
@@ -0,0 +1,74 @@
+package com.avs.battleship.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+abstract class SquareView : View {
+
+    protected var squareWidth = 0.0
+    protected var screenHeight = 0.0
+    protected var screenWidth = 0.0
+    protected lateinit var paint: Paint
+    private val squaresCount = 10
+    private val lineWidth = 1f
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        paint = Paint()
+        paint.color = Color.BLACK
+        paint.strokeWidth = lineWidth
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        paint.color = Color.BLACK
+        paint.strokeWidth = lineWidth
+        drawHorizontalLines(canvas!!)
+        drawVerticalLines(canvas)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        squareWidth = (MeasureSpec.getSize(widthMeasureSpec).toDouble() / squaresCount)
+        screenHeight = MeasureSpec.getSize(heightMeasureSpec).toDouble()
+        screenWidth = MeasureSpec.getSize(widthMeasureSpec).toDouble()
+    }
+
+
+    private fun drawHorizontalLines(canvas: Canvas) {
+        for (i in 1..squaresCount) {
+            canvas.drawLine(
+                0f, (screenHeight - squareWidth * i).toFloat(), screenWidth.toFloat(),
+                (screenHeight - squareWidth * i).toFloat(), paint
+            )
+        }
+    }
+
+    private fun drawVerticalLines(canvas: Canvas) {
+        for (i in 1..squaresCount) {
+            canvas.drawLine(
+                i * squareWidth.toFloat(),
+                0f,
+                i * squareWidth.toFloat(),
+                screenHeight.toFloat(),
+                paint
+            )
+        }
+    }
+}
