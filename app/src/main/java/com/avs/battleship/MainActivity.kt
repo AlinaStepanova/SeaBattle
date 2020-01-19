35b51d13f367571e433845fe7c7c088798eea328
AlinaStepanova <stepanovaalina15@gmail.com>
2020-01-19 20:07:36 +0200

    add data binding

--- app/src/main/java/com/avs/battleship/MainActivity.kt
+++ app/src/main/java/com/avs/battleship/MainActivity.kt
@@ -2,11 +2,18 @@ package com.avs.battleship
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.avs.battleship.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.buttonFire.setOnClickListener {  }
+        binding.buttonGenerate.setOnClickListener {  }
+        binding.buttonStart.setOnClickListener {  }
     }
 }
