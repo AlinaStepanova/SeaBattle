3d6d1838711cd045b92a2d800e7c605f220625f9
AlinaStepanova <stepanovaalina15@gmail.com>
2020-01-20 22:46:10 +0200

    create custom on touch listener

--- app/src/main/java/com/avs/battleship/MainActivity.kt
+++ app/src/main/java/com/avs/battleship/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.avs.battleship
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.avs.battleship.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val customOnTouchListener = View.OnTouchListener { v: View, event: MotionEvent ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                v.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.black
+                    )
+                )
+                if (v is TextView) v.setTextColor(ContextCompat.getColor(this, R.color.white))
+            }
+            MotionEvent.ACTION_UP -> {
+                v.background =
+                    ContextCompat.getDrawable(this, R.drawable.square_background)
+                if (v is TextView)
+                    v.setTextColor(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.greyDefault
+                        )
+                    )
+                v.performClick()
+            }
+        }
+        true
+    }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.buttonFire.setOnClickListener {  }
-        binding.buttonGenerate.setOnClickListener {  }
-        binding.buttonStart.setOnClickListener {  }
+
+        binding.buttonGenerate.setOnTouchListener(customOnTouchListener)
+        binding.buttonFire.setOnTouchListener(customOnTouchListener)
+        binding.buttonStart.setOnTouchListener(customOnTouchListener)
     }
 }
