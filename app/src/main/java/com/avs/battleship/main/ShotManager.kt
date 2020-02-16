e081d716a156de173635c308c6a9f96d93b868ef
Alina_Stepanova <stepanovaalina15@gmail.com>
2020-02-16 17:32:27 +0200

    update shot manager

--- app/src/main/java/com/avs/battleship/main/ShotManager.kt
+++ app/src/main/java/com/avs/battleship/main/ShotManager.kt
@@ -3,24 +3,26 @@ package com.avs.battleship.main
 import android.graphics.Point
 import com.avs.battleship.*
 import com.avs.battleship.battle_field.BaseBattleField
+import com.avs.battleship.battle_field.Cell
 import com.avs.battleship.battle_field.CellState
 
 class ShotManager {
 
     private val battleField = BaseBattleField()
 
-    lateinit var latestPoint: Point
+    private lateinit var latestCell: Cell
 
     fun getRandomPoint(): Point {
+        latestCell = Cell()
         do {
-            latestPoint = Point((0 until SQUARES_COUNT).random(), (0 until SQUARES_COUNT).random())
-        } while (!battleField.isCellFreeToBeSelected(latestPoint))
-        return latestPoint
+            latestCell.setCoordinates((0 until SQUARES_COUNT).random(), (0 until SQUARES_COUNT).random())
+        } while (!battleField.isCellFreeToBeSelected(latestCell.getPoint()))
+        return latestCell.getPoint()
     }
 
     fun handleShot(shipHit: Boolean) {
         battleField.setCellState(
-            latestPoint,
+            latestCell.getPoint(),
             if (shipHit) CellState.SHOT_SUCCESS
             else CellState.SHOT_FAILURE
         )
