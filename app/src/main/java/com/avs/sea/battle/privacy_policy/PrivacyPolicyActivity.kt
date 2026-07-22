8669896c7b09e9ffab3cc534e81d3086123a55bf
AlinaStepanova <alina.stepanova.work@gmail.com>
2026-07-22 15:37:43 +0200

    migrate privacy policy screen to compose with webview back handling

--- app/src/main/java/com/avs/sea/battle/privacy_policy/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/avs/sea/battle/privacy_policy/PrivacyPolicyActivity.kt
@@ -1,22 +1,50 @@
 package com.avs.sea.battle.privacy_policy
 
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import com.avs.sea.battle.PRIVACY_POLICY_URL
-import com.avs.sea.battle.R
-import com.avs.sea.battle.databinding.ActivityPrivacyPolicyBinding
+import com.avs.sea.battle.ui.theme.SeaBattleTheme
 
-class PrivacyPolicyActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityPrivacyPolicyBinding
+class PrivacyPolicyActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_privacy_policy)
-        val webView = binding.wvPrivacyPolicy
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl(PRIVACY_POLICY_URL)
+        enableEdgeToEdge()
+        setContent {
+            SeaBattleTheme {
+                var webView by remember { mutableStateOf<WebView?>(null) }
+                var canGoBack by remember { mutableStateOf(false) }
+                BackHandler(enabled = canGoBack) { webView?.goBack() }
+                AndroidView(
+                    modifier = Modifier.fillMaxSize().safeDrawingPadding(),
+                    factory = { context ->
+                        WebView(context).apply {
+                            webViewClient = object : WebViewClient() {
+                                override fun doUpdateVisitedHistory(
+                                    view: WebView?, url: String?, isReload: Boolean
+                                ) {
+                                    canGoBack = view?.canGoBack() == true
+                                }
+                            }
+                            loadUrl(PRIVACY_POLICY_URL)
+                            webView = this
+                        }
+                    },
+                )
+            }
+        }
     }
 }
